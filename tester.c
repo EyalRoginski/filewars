4110ebb0a1d4707ceee4d80d269d5aff94f86dee
Eyal Roginski <eyalrog1@gmail.com>
2024-07-31 18:57:31 +0300

    gitignore change

--- tester.c
+++ tester.c
@@ -4,7 +4,7 @@ int main() {
     write(STDOUT_FILENO, first, sizeof(first));
     char second[] = "add\00012\0003\0";
     write(STDOUT_FILENO, second, sizeof(second));
-    char third[] = "write\0testing\0abcef\0\0";
+    char third[] = "write\0testing.test\0abcef\0";
     write(STDOUT_FILENO, third, sizeof(third));
     return 0;
 }
